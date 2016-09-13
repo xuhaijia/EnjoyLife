84d98903100301ff8e733aa5b02843e6e39c59bf
dllo <dllo@dllodeMac-mini-135.local>
2016-09-13 21:01:45 +0800

    gbb-day1

---
+++ app/src/main/java/com/lanou/xuhaijia/enjoylife/news/fragment/HardlinesFragment.java
@@ -0,0 +1,24 @@
+package com.lanou.xuhaijia.enjoylife.news.fragment;
+
+import com.lanou.xuhaijia.enjoylife.R;
+import com.lanou.xuhaijia.enjoylife.base.BaseFragment;
+
+/**
+ * Created by 国冰冰 on 16/9/13.
+ */
+public class HardlinesFragment extends BaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_hardlines;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/lanou/xuhaijia/enjoylife/news/fragment/recreationFragment.java
@@ -0,0 +1,24 @@
+package com.lanou.xuhaijia.enjoylife.news.fragment;
+
+import com.lanou.xuhaijia.enjoylife.R;
+import com.lanou.xuhaijia.enjoylife.base.BaseFragment;
+
+/**
+ * Created by 国冰冰 on 16/9/13.
+ */
+public class recreationFragment extends BaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_recraction;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
