92b65b2a61515b7baee047cd95f04364136ebc15
dllo <1752066766@qq.com>
2016-09-18 19:46:30 +0800

    二级布局

--- app/src/main/java/com/lanou/xuhaijia/enjoylife/picture/picture/PictureFragment.java
+++ app/src/main/java/com/lanou/xuhaijia/enjoylife/picture/picture/PictureFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
@@ -49,17 +50,7 @@ public class PictureFragment extends BaseFragment {
 
     @Override
     protected void initData() {
-        overview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("PictureFragment", "点击");
-                     Intent intent = new Intent(getActivity(), DetailsActivity.class);
-                     intent.putExtra("ID", urlAdd);
-                     startActivity(intent);
-            }
-        });
-        overview.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        overview.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -92,6 +83,7 @@ public class PictureFragment extends BaseFragment {
 
         stack = new OverviewAdapter<ViewHolder<View, Integer>, Integer>(models) {
 
+            private RelativeLayout layout;
             private ImageView avatar;
             private ImageView image;
             private TextView sign;
@@ -111,7 +103,17 @@ public class PictureFragment extends BaseFragment {
                     @Override
                     public void onSuccess(PictureBean pictureBean) {
                         int i = viewHolder.getPosition();
+
                         urlAdd = pictureBean.getData().getArticles().get(i).getId();
+                        layout = (RelativeLayout) viewHolder.itemView.findViewById(R.id.fragment_picture_item_layout);
+                        layout.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(getActivity(),DetailsActivity.class);
+                                intent.putExtra("ID",urlAdd);
+                                startActivity(intent);
+                            }
+                        });
                         title = (TextView) viewHolder.itemView.findViewById(R.id.fragment_picture_item_title);
                         titleSub = (TextView) viewHolder.itemView.findViewById(R.id.fragment_picture_item_title_sub);
                         sign = (TextView) viewHolder.itemView.findViewById(R.id.fragment_picture_item_sign);
@@ -130,8 +132,6 @@ public class PictureFragment extends BaseFragment {
                     }
                 });
             }
-
-
         };
 
         overview.setTaskStack(stack);
@@ -146,6 +146,9 @@ public class PictureFragment extends BaseFragment {
     public void onDestroy() {
         super.onDestroy();
     }
-
+/**
+ * ArrayList 采用的是数组形式来保存对象的，这种方式将对象放在连续的位置中，所以最大的缺点就是插入删除时非常麻烦
+ * LinkedList 采用的将对象存放在独立的空间中，而且在每个空间中还保存下一个链接的索引  但是缺点就是查找非常麻烦 要丛第一个索引开始
+ */
 
 }
